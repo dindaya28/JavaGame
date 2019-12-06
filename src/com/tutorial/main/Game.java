4923aa38e45f1a1e6bcc3c726686a7f800d8b8b9
Dinushan Dayarathna <dindaya95@gmail.com>
2019-12-06 13:57:51 -0500

    Basic game window created, game loop and fps counter running

--- src/com/tutorial/main/Game.java
+++ src/com/tutorial/main/Game.java
@@ -1,5 +1,98 @@
 package com.tutorial.main;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
+// main class, go to class
+public class Game extends Canvas implements Runnable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1550691097823471818L;
+	
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+	//Entire game is going to run within this thread
+	private Thread thread;
+	//Check to see if the current thread is running or not
+	private boolean running = false;
+	
+	public Game() {
+		new Window(WIDTH, HEIGHT, "Learning to Build a Game!", this);
+	}
+
+	public synchronized void start() {
+		//this is referring to the current instance of the game
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		try {
+			//Stops the thread
+			thread.join();
+			running = false;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Using a popular game loop to help with the running of the game, essentially the heart beat of the game
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		
+		while (running ) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1 ) {
+				tick();
+				delta--;
+			}
+			
+			if (running)
+				render();
+			frames++;
+			
+			if ((System.currentTimeMillis() - timer) > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String args[]) {
+		new Game();
+	}
 }
--- src/com/tutorial/main/Window.java
+++ src/com/tutorial/main/Window.java
@@ -1,5 +1,33 @@
 package com.tutorial.main;
 
-public class Window {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window  extends Canvas{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -240840600533728354L;
+	
+	public Window(int width, int height, String title, Game game) {
+		//JFrame is the frame of our window
+		JFrame frame = new JFrame(title);
+		
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		
+		//Allows for the exit button to actually work, instead of having the threads continuously running in the background after window close
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		//Adding the game to the frame, without it the game will not show on the frame
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();
+	}
 
 }
